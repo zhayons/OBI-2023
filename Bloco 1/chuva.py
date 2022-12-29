907f7633b26285a8e106fde965bbd0d6af614208
zhayon <89433360+zhayon@users.noreply.github.com>
2022-12-28 22:17:08 -0300

    e lá vamos nós

--- Bloco 1/chuva.py
+++ Bloco 1/chuva.py
@@ -4,14 +4,6 @@ section = int(input())
 values = [int(input()) for i in range(section)]
 output = 0
 
-while len(values) > 2:
-    indice = 0
-    for i in range(1, len(values)):
-        if values[i] >= values[0]:
-            indice = i
-            output += len(values[0:i + 1]) - 2
-            break
 
-    del values[0:indice + 1]
 
 print(output)
