a06a3faa3cb6bee663540f0a501755708a940ed3
zhayon <89433360+zhayon@users.noreply.github.com>
2022-12-28 22:16:09 -0300

    tudo errado

--- Bloco 1/chuva.py
+++ Bloco 1/chuva.py
@@ -1,1 +1,17 @@
 # exercicio de: https://olimpiada.ic.unicamp.br/pratique/p2/2016/f1/chuva/
+
+section = int(input())
+values = [int(input()) for i in range(section)]
+output = 0
+
+while len(values) > 2:
+    indice = 0
+    for i in range(1, len(values)):
+        if values[i] >= values[0]:
+            indice = i
+            output += len(values[0:i + 1]) - 2
+            break
+
+    del values[0:indice + 1]
+
+print(output)
