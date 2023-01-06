5c7cc72cd303eef8c0dd6500c62f30b4b2a2e740
zhayon <89433360+zhayon@users.noreply.github.com>
2023-01-05 23:15:25 -0300

    "cofre" resolvido

--- Bloco 3/cofre.py
+++ Bloco 3/cofre.py
@@ -1,1 +1,18 @@
 # exercício de: https://olimpiada.ic.unicamp.br/pratique/p1/2017/f1/cofre/
+
+N_sequence = int(input().split(" ")[0])
+sequence = [int(i) for i in input().split(" ")]
+positions = [int(i) for i in input().split(" ")]
+key = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+position = -1
+
+for i in positions:
+    if i-1 > position:
+        for j in range(position+1, i):
+            key[sequence[j]] += 1
+    else:
+        for j in range(position-1, i-2, -1):
+            key[sequence[j]] += 1
+    position = i - 1
+
+print(key)
