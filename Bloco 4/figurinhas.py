8a5d73198068a8a1225c01ec64e46a8b512d3c90
zhayon <89433360+zhayon@users.noreply.github.com>
2023-01-15 13:46:30 -0300

    "figurinhas" solved

--- Bloco 4/figurinhas.py
+++ Bloco 4/figurinhas.py
@@ -1,1 +1,12 @@
 # exercício de: https://olimpiada.ic.unicamp.br/pratique/p2/2018/f1/figurinhas/
+
+N_figurinhas, N_carimbadas, N_compradas = input().split(" ")
+carimbadas = input().split(" ")
+compradas = input().split(" ")
+output = int(N_carimbadas)
+
+for i in carimbadas:
+    if i in compradas:
+        output -= 1
+
+print(output)
