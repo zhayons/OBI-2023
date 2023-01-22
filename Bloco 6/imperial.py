4627a6d720dd8f1d0450d50181205314467f38a1
zhayon <89433360+zhayon@users.noreply.github.com>
2023-01-22 02:42:29 -0300

    "imperial" solved

--- Bloco 6/imperial.py
+++ Bloco 6/imperial.py
@@ -1,34 +1,41 @@
 # exercício de: https://olimpiada.ic.unicamp.br/pratique/p2/2019/f1/imperial/
 
-def consecutivo(posicao_sequencia):
+def valida_sequencia(items):
+    validos = 1
+    comparacao = items[0]
+    for i in items[1:]:
+        if i != comparacao + 1:
+            comparacao = i
+            validos += 1
+    return validos
 
-    for i in posicao_sequencia:
 
+def conta_ocorrencia(items):
+    posicoes = []
+    for i in range(N_sequencia):
+        if sequencia[i] in items:
+            posicoes.append(i)
+
+    N_posicoes_validas = valida_sequencia(posicoes)
+
+    return N_posicoes_validas
 
 
 N_sequencia = int(input())
 sequencia = [int(input()) for i in range(N_sequencia)]
 
 valores_unicos = []
-posicoes = []
 output = 0
 
 for i in range(N_sequencia):
     if sequencia[i] not in valores_unicos:
         valores_unicos.append(sequencia[i])
-        i_posicoes = []
-        for j in range(i, N_sequencia):
-            if sequencia[i] == sequencia[j]:
-                i_posicoes.append(j)
-        posicoes.append(i_posicoes)
 
 N_valores_unicos = len(valores_unicos)
 for i in range(N_valores_unicos):
-    N_posicoes = len(posicoes[i])
-    if N_posicoes > output:
-        output = N_posicoes
+    for j in range(i, N_valores_unicos):
+        i_sequencia = conta_ocorrencia([valores_unicos[i], valores_unicos[j]])
+        if i_sequencia > output:
+            output = i_sequencia
 
-
-print(N_sequencia, sequencia)
-print(valores_unicos, posicoes)
 print(output)
