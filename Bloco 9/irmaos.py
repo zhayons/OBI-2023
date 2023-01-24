24174b2f164268a6438ce2244d3e0c7c0bdc378b
zhayon <89433360+zhayon@users.noreply.github.com>
2023-01-24 02:48:12 -0300

    "irmaos" add &  solved

---
+++ Bloco 9/irmaos.py
@@ -0,0 +1,7 @@
+# exercício de: https://olimpiada.ic.unicamp.br/pratique/p2/2020/f1/irmaos/
+# OBI, vc já foi melhor.
+
+N_casula= int(input())
+N_otavio = int(input())
+
+print(N_otavio+(N_otavio-N_casula))
